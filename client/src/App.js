490435835ec5c46ea9b04e2685ef2af98a290907
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-12 13:56:01 +0100

    Add client files and dependencies

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+
+import Registration from './components/Registration';
+import Login from './components/Login';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route path='/register' element={<Registration />} />
+      <Route path='/login' element={<Login />} />
+    </Route>
+  )
+)
+
+export default function App() {
+
+  return (
+    <div>
+        <RouterProvider router={router} />
+    </div>
+  )
+}
