4f0300015d37c0ce45c368977f0bc80cc920aebd
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-12 10:51:14 +0100

    Update Program.cs, add appsettings.sample.json

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,9 +1,22 @@
+using Bardi_demo_project.Models;
+using Bardi_demo_project.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IVectorService, VectorService>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
