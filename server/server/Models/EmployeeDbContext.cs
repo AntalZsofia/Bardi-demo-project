bc954b86177dbda6f6a5d7d9d23d3c880876fb8b
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-12 10:53:40 +0100

    Add EmployeeDbContext

---
+++ server/server/Models/EmployeeDbContext.cs
@@ -0,0 +1,27 @@
+using Bardi_demo_project.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bardi_demo_project.Models;
+
+public class EmployeeDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+
+    public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>().HasData(
+            new Employee
+            {
+                IdentificationNumber = 1,
+                Name = "John",
+                VectorOfImage = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            }
+        );
+    }
+}
+    
+    
