b9218e4bc4b887776f03cd167dad45935ce86359
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-13 14:30:17 +0100

    Update entity, dto, result

--- server/server/Models/Entities/Employee.cs
+++ server/server/Models/Entities/Employee.cs
@@ -1,7 +1,13 @@
-﻿namespace Bardi_demo_project.Services;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bardi_demo_project.Services;
 
 public class Employee
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     public string Name { get; set; }
     public int IdentificationNumber { get; set; }
     public double[] VectorOfImage { get; set; }
--- server/server/Models/RequestDto/RegisterEmployeeDto.cs
+++ server/server/Models/RequestDto/RegisterEmployeeDto.cs
@@ -7,9 +7,7 @@ public class RegisterEmployeeDto
     [Required]
     [MinLength(4, ErrorMessage = "Username must be at least 4 characters long.")]
     public string Name { get; set; }
-    
-    public int IdentificationNumber { get; set; }
-    
+
     [Required]
     public IFormFile Image { get; set; }
 }
